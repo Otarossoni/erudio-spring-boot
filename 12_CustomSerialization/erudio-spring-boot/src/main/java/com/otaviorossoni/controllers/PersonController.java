9f6fedbd63e784950559d3883763177a6b562edb
Otávio Monteiro Rossoni <otarossoni@gmail.com>
2025-10-11 11:34:43 -0300

    Custom JSON serialization

---
+++ 12_CustomSerialization/erudio-spring-boot/src/main/java/com/otaviorossoni/controllers/PersonController.java
@@ -0,0 +1,58 @@
+package com.otaviorossoni.controllers;
+
+import com.otaviorossoni.data.dto.PersonDTO;
+import com.otaviorossoni.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/person/v1")
+public class PersonController {
+
+    @Autowired
+    private PersonServices service;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<PersonDTO> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonDTO findById(@PathVariable("id") Long id) {
+        var person = service.findById(id);
+        person.setBirthDate(new Date());
+        // person.setPhoneNumber("+55 (54) 9 9999-9999");
+        person.setPhoneNumber("");
+        person.setLastName(null);
+        person.setSensitiveData("PASSWORD");
+        return person;
+    }
+
+    @PostMapping(
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public PersonDTO create(@RequestBody PersonDTO person) {
+        return service.create(person);
+    }
+
+    @PutMapping(
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public PersonDTO update(@RequestBody PersonDTO person) {
+        return service.update(person);
+    }
+
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
